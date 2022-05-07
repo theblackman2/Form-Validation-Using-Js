4faf9652bd1d6c8f9a271fb1eff58cff63448295
theblackman2 <paskasonga@gmail.com>
2022-05-07 22:23:09 +0100

    Finished: validate email adress

--- app.js
+++ app.js
@@ -10,25 +10,47 @@ const password = document.querySelector('password')
 
 const passwordConfirm = document.querySelector('password-confirm')
 
+function validateEmail (email){
+    let regex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/
+
+    if (regex.test(email)) {
+        return true
+    } else {
+        return false
+    }
+}
+  
+
 const form = document.querySelector('#form')
 
 
 form.addEventListener("submit", function(e){
 
+    // validate username
     if(userName.value.length < 3 || userName.value.length > 25){
 
-        let userNameMessage = userName.nextElementSibling
+        
 
-        userNameMessage.classList.add("visible")
+        userName.nextElementSibling.classList.add("visible")
 
         userName.classList.add("red")
 
         e.preventDefault()
 
+    }else{
+        userName.classList.add("match")
     }
 
+    //validate email adress
+    if(!validateEmail(email.value)){
+        email.nextElementSibling.classList.add("visible")
 
+        email.classList.add("red")
 
-
+        e.preventDefault()
+    }else{
+        email.classList.add("match")
+    }
 
 })
+
